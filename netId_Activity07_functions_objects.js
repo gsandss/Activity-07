bfddbfa81f341625c93941fc95e80029902028a4
gsandss <gavinsands11@gmail.com>
2026-02-19 23:12:43 -0600

    final commit

--- netId_Activity07_functions_objects.js
+++ netId_Activity07_functions_objects.js
@@ -26,10 +26,14 @@ console.log(`The max between ${n1} and ${n2} is :`, maxOfTwo(n1,n2));
 
 function maxOfArray(array){
 
+    let max = array[0];
 
-    for(let i in array){
-        
-    }
+    for(let i =1; i < array.length; i++){
+        if(array[i] > max){
+            max = array[i];
+        }
+    }   
+    return max;
 }
 
 // Exercise 3
@@ -60,12 +64,58 @@ let circle={
     radius: 2,
     area: function(){
         return Math.PI * this.radius * this.radius;
-    }
+    },
 }
 
 console.log(circle.area());
 
 // Exercise 5
+
+let circle={
+    radius: 2,
+
+    get radiusValue(){
+        return this.radius;
+    },
+
+    set radiusValue(value){
+        if (value > 0){
+            this.radius=value;
+        }
+        else{
+            console.log('Radius must be positive');
+        }
+
+    },
+
+    area: function(){
+        return Math.PI * this.radius * this.radius;
+    },
+}
+
+// Exercise 6
+
+let circle={
+    radius: 2,
+
+    getRadiusValue: function(){
+        return this.radius;
+    },
+
+    setRadiusValue: function(value){
+        if (value > 0){
+            this.radius=value;
+        }
+        else{
+            console.log('Radius must be positive');
+        }
+
+    },
+
+    area: function(){
+        return Math.PI * this.radius * this.radius;
+    },
+}
 
 
 
